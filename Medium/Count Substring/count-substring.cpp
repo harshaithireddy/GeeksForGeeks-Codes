7dcdaa703dfcc1ca0e9dbac556c00ff2aee65dd9
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-04-25 21:02:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Substring/count-substring.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countSubstring(string s) {
+        // Code here
+        int a = -1, b = -1, c = -1;
+        int cnt = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == 'a') a = i;
+            if(s[i] == 'b') b = i;
+            if(s[i] == 'c') c = i;
+            
+            if(a != -1 && b != -1 && c != -1) {
+                cnt += min(min(a, b), c) + 1;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution obj;
+        cout << obj.countSubstring(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
