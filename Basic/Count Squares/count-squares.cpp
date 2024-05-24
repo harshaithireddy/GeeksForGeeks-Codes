1a564e9d766655c6b81ab2bbe3c4dae042e66cee
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-05-24 21:37:15 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count Squares/count-squares.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        int cnt = 0;
+        int i = 1;
+        while(i < sqrt(N)) {
+            cnt++;
+            i++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
