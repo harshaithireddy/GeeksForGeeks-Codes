a4119ca068cc8b68b4a83157b9f2bceb35b0d81e
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-06-24 21:03:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        long long l = 2+n-1;
+        long long res = n - abs(q-l);
+        
+        if(res < 0) return 0;
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
