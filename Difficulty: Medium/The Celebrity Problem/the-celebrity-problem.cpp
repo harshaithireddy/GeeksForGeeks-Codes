5be1d1b6398c145f89c3149e705a2ed0ea9e0907
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-08-03 20:22:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        // code here
+        int row = mat.size();
+        int col = mat[0].size();
+        int res = -1;
+        for(int i = 0; i < col; i++) {
+            int cnt = 0;
+            for(int j = 0; j < row; j++) {
+                if(mat[j][i] == 1 && mat[i][j] == 0) cnt++;
+                if(cnt == col - 1) res = i;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.celebrity(M) << endl;
+    }
+}
+
+// } Driver Code Ends
