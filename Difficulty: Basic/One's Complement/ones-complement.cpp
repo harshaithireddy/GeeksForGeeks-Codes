77aafda23ecc7ecbd2b976a4da7721b8efded113
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-08-22 21:19:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/One's Complement/ones-complement.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{
+public:
+    int onesComplement(int N){
+        //code here
+        int i = 0, res = 0;
+        while(N) {
+            int temp = N & 1;
+            if(!temp) res += pow(2, i);
+            N = N >> 1;
+            i++;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        cout<<ob.onesComplement(n)<<"\n";
+    }
+}
+// } Driver Code Ends
