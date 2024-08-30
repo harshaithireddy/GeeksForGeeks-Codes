c727ab2ff56268c8484730ad0a6d02e7b3a8c66c
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2024-08-30 20:34:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    void solve(int col, vector<int>& board, vector<vector<int>>& res, 
+           vector<int>& leftrow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal, int n) {
+        if(col == n) {
+            vector<int> oneBased(n);
+            for(int i = 0; i < n; ++i) {
+                oneBased[i] = board[i] + 1;
+            }
+            res.push_back(oneBased);
+            return;
+        }
+        
+        for(int row = 0; row < n; row++) {
+            if(leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
+                board[col] = row;
+                leftrow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+                
+                solve(col + 1, board, res, leftrow, upperDiagonal, lowerDiagonal, n);
+                
+                leftrow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
+            }
+        }
+    }
+    
+    vector<vector<int>> nQueen(int n) {
+        vector<vector<int>> res;
+        vector<int> board(n, -1);
+    
+        vector<int> leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
+        
+        solve(0, board, res, leftrow, upperDiagonal, lowerDiagonal, n);
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        vector<vector<int>> ans = ob.nQueen(n);
+        if(ans.size() == 0)
+            cout<<-1<<"\n";
+        else {
+            for(int i = 0;i < ans.size();i++){
+                cout<<"[";
+                for(int u: ans[i])
+                    cout<<u<<" ";
+                cout<<"] ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
