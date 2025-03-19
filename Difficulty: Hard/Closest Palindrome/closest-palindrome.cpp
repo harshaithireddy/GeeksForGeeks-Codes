a46528041a0149e49fa24a6e91cd701a73e5cd38
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2025-03-19 10:47:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Closest Palindrome/closest-palindrome.cpp
+++ Difficulty: Hard/Closest Palindrome/closest-palindrome.cpp
@@ -3,62 +3,43 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
-private:
-    long long generatePalindrome(long long leftHalf, bool isEvenLength) {
-        long long palindrome = leftHalf;
-        if (!isEvenLength) leftHalf /= 10;
-        while (leftHalf > 0) {
-            palindrome = palindrome * 10 + leftHalf % 10;
-            leftHalf /= 10;
-        }
-        return palindrome;
-    }
-
-    bool isPalindrome(long long num) {
-        std::string s = std::to_string(num);
-        int n = s.size();
-        for (int i = 0; i < n / 2; i++) {
-            if (s[i] != s[n - i - 1]) return false;
-        }
-        return true;
-    }
-
 public:
-    long long int closestPalindrome(long long int num) {
-        long long number = num;
-
-        if (number <= 10) return number;
-        if (number == 11) return 9;
-
-        if (isPalindrome(number)) return number;
-
-        int length = to_string(number).length();
-        long long leftHalf = stoll(to_string(number).substr(0, (length + 1) / 2));
-
-        std::vector<long long> palindromes(5);
-        palindromes[0] = generatePalindrome(leftHalf - 1, length % 2 == 0);
-        palindromes[1] = generatePalindrome(leftHalf, length % 2 == 0);
-        palindromes[2] = generatePalindrome(leftHalf + 1, length % 2 == 0);
-        palindromes[3] = pow(10, length - 1) - 1;
-        palindromes[4] = pow(10, length) + 1;
-
-        long long nearestPalindrome = 0;
-        long long minDifference = LLONG_MAX;
-
-        for (long long palindrome : palindromes) {
-            if (palindrome == number) continue;
-            long long difference = abs(palindrome - number);
-            if (difference < minDifference || (difference == minDifference && palindrome < nearestPalindrome)) {
-                minDifference = difference;
-                nearestPalindrome = palindrome;
-            }
+    long long int generate(long long int n, int i) {
+        long long int p = n;
+        if(i) n /= 10;
+        while(n) {
+            p = p * 10 + (n % 10);
+            n /= 10;
         }
-
-        return nearestPalindrome;
+        return p;
     }
+    
+	long long int closestPalindrome(long long int num){
+	    // Code here
+	    if(num <= 9) return num;
+	    string s = to_string(num);
+	    int n = s.size();
+	    
+	    string left = s.substr(0, ((n + 1) / 2));
+	    long long int res = generate(stoll(left), n % 2);
+	    vector<long long int> V;
+	    
+	    V.push_back(generate(stoll(left) + 1, 0));
+	    V.push_back(generate(stoll(left) + 1, 1));
+	    V.push_back(generate(stoll(left) - 1, 0));
+	    V.push_back(generate(stoll(left) - 1, 1));
+	    
+	    for(auto it : V) {
+	        if(abs(num - res) > abs(num - it)) res = it;
+	        else if(abs(num - res) == abs(num - it)) res = min(res, it);
+	    }
+	    return res;
+	}
 
 };
+
 
 //{ Driver Code Starts.
 int main(){
@@ -70,7 +51,9 @@ int main(){
 		Solution obj;
 		long long int ans = obj.closestPalindrome(num);
 		cout << ans <<"\n";
-	}
+	
+cout << "~" << "\n";
+}
 	return 0;
 }
 // } Driver Code Ends
