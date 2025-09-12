99e33c0029fe44a6502b74fbd5d46a591080448c
Ithireddy Harsha Vardhan <119839919+harshaithireddy@users.noreply.github.com>
2025-09-12 19:19:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
@@ -0,0 +1,19 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        
+        int res = arr[n - 1] - arr[0];
+        
+        for(int i = 1; i < n; i++) {
+            int mini = min(arr[0] + k, arr[i] - k); 
+            int maxi = max(arr[i-1] + k , arr[n-1] - k); 
+            res = min(res, maxi - mini); 
+        }
+        return res; 
+    }
+};
